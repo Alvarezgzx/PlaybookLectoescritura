3bbc8028222d7c3d2a96996ea49494f572e8305f
Alvarezgzx <angel.alvarezg9@gmail.com>
2025-07-15 00:38:56 -0600

    Fix: Correct comment casing in vite.config.ts for consistency

--- App-lectoescritura/vite.config.ts
+++ App-lectoescritura/vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // La base debe coincidir con la ruta donde vive la aplicación.
+  // La base DEBE coincidir con la ruta donde vive la aplicación.
   base: '/asistente-ia/', 
   resolve: {
     alias: {
